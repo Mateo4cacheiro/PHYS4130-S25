0657109d497e5bd409431c1429815e44d1b613fb
mateo4cacheiro <mateo4cacheiro@gmail.com>
2025-01-22 17:52:55 -0600

    made a plot

--- p0-GUI/mateo4cacheiro/code/program.py
+++ p0-GUI/mateo4cacheiro/code/program.py
@@ -1,2 +1,19 @@
-Hello small World
-Hello Big World
+#******************************************************************************************
+#	 Title:  	program.py
+# 	 Date:		1/22/2025   
+#    Class:     PHYS 4130
+#	
+#	Purpose:    Create a Simple GUI to complete p0-GUI assignment
+#******************************************************************************************
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as npy
+
+xpoint = npy.array([0, 6])
+ypoint = npy.array([0, 250])
+
+plt.plot(xpoint, ypoint)
+plt.show
+
+
