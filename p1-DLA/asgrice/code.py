f8219d771ec277e4c3aa9f3801d71b7c57a83e8d
asgrice <asgrice13@gmail.com>
2025-02-06 08:47:52 -0600

    Update code.py

--- p1-DLA/asgrice/code.py
+++ p1-DLA/asgrice/code.py
@@ -7,33 +7,42 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import time
 
+
 # Create grid
-x = 20
-spawn_rad = 18
-Nmax = 30
+x = 1000
+spawn_rad = 10
+Nmax = 10000
 N = 1
 grid = np.zeros((x,x))
 grid[x//2,x//2] = 1
 i = 0
 
 
+def generate_on_circle(spawn_rad, x0, y0):
+
+    angle = np.random.rand() * 2*np.pi
+    xval = round(spawn_rad * np.cos(angle)) + x0
+    yval = round(spawn_rad * np.sin(angle)) + y0
+
+    return xval, yval
+
 def generate_particle(spawn_rad):
     s = np.random.randint(low=0, high=4) #choose integer from 0 to 3 to determine which side to generate particle on
     if(s == 0): #generate particle at top of grid
-        x0 = np.random.randint(low = x - spawn_rad, high = spawn_rad)
+        x0 = np.random.randint(low = x//2 - spawn_rad, high = x // 2 + spawn_rad)
         y0 = spawn_rad
         return x0, y0
     elif(s == 1): #right side of grid
         x0 = spawn_rad
-        y0 = np.random.randint(low = x - spawn_rad, high = spawn_rad)
+        y0 = np.random.randint(low = x//2 - spawn_rad, high = x//2 + spawn_rad)
         return x0, y0
     elif(s == 2): #bottom of grid
-        x0 = np.random.randint(low = x - spawn_rad, high = spawn_rad)
+        x0 = np.random.randint(low = x//2 - spawn_rad, high = x // 2 + spawn_rad)
         y0 = spawn_rad
         return x0, y0
     elif(s == 3): #left side of grid
         x0 = x - spawn_rad
-        y0 = np.random.randint(low = x - spawn_rad, high = spawn_rad)
+        y0 = np.random.randint(low = x//2 - spawn_rad, high = x // 2 + spawn_rad)
         return x0, y0
 
 
@@ -41,7 +50,7 @@ def generate_particle(spawn_rad):
 
 # determines whether there is a nonzero point in vicinity of another nonzero point
 def get_coords(grid, x0, y0):
-    if (np.sum(grid[x0-1:x0+2, y0-1:y0+2]) > 1 ):
+    if (np.sum(grid[x0-1:x0+2, y0-1:y0+2]) > 0 ):
         return True
     else:
         return False
@@ -81,7 +90,7 @@ def step(a, b):
 #     i += 1
 
 
-
+#------Diagnostic-------
 # grid = np.zeros((x,x))
 # for m in range(1000):
 #    x2, y2 = generate_particle(x)
@@ -89,30 +98,43 @@ def step(a, b):
 #    print(f'{x2},{y2}')
 
 
+kill_rad = spawn_rad+6
+while(N < Nmax):
+    stuck = False
+    x1, y1 = generate_on_circle(spawn_rad, x//2, x//2)
+    while not stuck:
+        x1, y1 = step(x1, y1)
+        dist = np.sqrt(((x//2) - x1)**2 + ((x//2) - y1)**2)
+        if(dist > kill_rad):
+            x1, y1 = generate_on_circle(spawn_rad, x//2, x//2)
+        stuck = get_coords(grid,x1,y1)
+    if stuck:
+        grid[x1, y1] = 1
+        N+=1
+        rad = np.sqrt(((x//2) - x1)**2 + ((x//2) - y1)**2)
+        if(rad >= spawn_rad):
+            spawn_rad = rad + 10
+            kill_rad = spawn_rad + 6
+
+plt.imshow(grid)
+plt.show()
+# plt.draw()
+# plt.pause(0.0001)
+
+        
+
+
+
+
+# -----------Diagnostic----------------------
 # while(N < Nmax):
-#     stuck = False
 #     x1, y1 = generate_particle(spawn_rad)
 #     if(x1 > x - 1 or x1 < 1 or y1 > x - 1 or y1 < 1):
 #         x1, y1 = generate_particle(spawn_rad)
-#     while not stuck:
-#         print(x1, y1)
-#         x1, y1 = step(x1, y1)
-#         if(x1 > x - 1 or x1 < 1 or y1 > x - 1 or y1 < 1):
-#             x1, y1 = generate_particle(spawn_rad)
-#         print(x1, y1)
-#         stuck = get_coords(grid,x1,y1)
-#     if stuck:
-#         N+=1
-#         rad = np.sqrt((x1^2)+(y1^2))
-        
-while(N < Nmax):
-    x1, y1 = generate_particle(spawn_rad)
-    if(x1 > x - 1 or x1 < 1 or y1 > x - 1 or y1 < 1):
-        x1, y1 = generate_particle(spawn_rad)
-    print(x1, y1)
-    x1, y1 = step(x1, y1)
-    print(x1, y1)
-    if(x1 > x - 1 or x1 < 1 or y1 > x - 1 or y1 < 1):
-        x1, y1 = generate_particle(spawn_rad)
+#     print(x1, y1)
+#     x1, y1 = step(x1, y1)
+#     print(x1, y1)
+#     if(x1 > x - 1 or x1 < 1 or y1 > x - 1 or y1 < 1):
+#         x1, y1 = generate_particle(spawn_rad)
 
-    N+=1
+#     N+=1
