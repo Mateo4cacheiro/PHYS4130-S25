8d4b52215d593f9a3cc47a1456a0e51dc251497c
asgrice <asgrice13@gmail.com>
2025-03-06 14:39:38 -0600

    Update code.py

--- p2-NInt/asgrice/code/code.py
+++ p2-NInt/asgrice/code/code.py
@@ -1,11 +1,10 @@
 # Author: Adam Grice
 # Project: P2-NInt
-# Date: 03-04-2025
+# Date: 03-06-2025
 
 import numpy as np
 import scipy
 import scipy.special
-from tabulate import tabulate
 import matplotlib.pyplot as plt
 
 
@@ -37,7 +36,105 @@ def gauss_quad(N, a, b):
     for i in range (N):
         roots, weights = scipy.special.roots_legendre(i+1)
         I += weights[i]*func(roots[i],0,2)
-        i += 1
 
     return I*du
-print(gauss_quad(1000, 0, 2))
+print(gauss_quad(10, 0, 2))
+
+
+min = -1.0
+max = 1.0
+step = 0.05
+# for n in range(6):
+#     Pn = scipy.special.legendre(n)
+#     x = np.arange(min,max+step,step)
+#     y = Pn(x)
+#     plt.plot(x, y)
+
+
+
+fig, axs = plt.subplots(4, 4)
+
+x = np.arange(min, max + step, step)
+
+P1 = scipy.special.legendre(1)
+P2 = scipy.special.legendre(2)
+P3 = scipy.special.legendre(3)
+P4 = scipy.special.legendre(4)
+
+y1 = P1(x)
+y2 = P2(x)
+y3 = P3(x)
+y4 = P4(x)
+
+#top row
+axs[0,0].plot(x, y1)
+axs[0,0].plot(x, y1)
+axs[0,0].plot(x, y1*y1)
+
+axs[0,1].plot(x, y1)
+axs[0,1].plot(x, y2)
+axs[0,1].plot(x, y1*y2)
+
+axs[0,2].plot(x, y1)
+axs[0,2].plot(x, y3)
+axs[0,2].plot(x, y1*y3)
+
+axs[0,3].plot(x, y1)
+axs[0,3].plot(x, y4)
+axs[0,3].plot(x, y1*y4)
+
+
+#2nd row
+axs[1,0].plot(x, y2)
+axs[1,0].plot(x, y1)
+axs[1,0].plot(x, y2*y1)
+
+axs[1,1].plot(x, y2)
+axs[1,1].plot(x, y2)
+axs[1,1].plot(x, y2*y2)
+
+axs[1,2].plot(x, y2)
+axs[1,2].plot(x, y3)
+axs[1,2].plot(x, y2*y3)
+
+axs[1,3].plot(x, y2)
+axs[1,3].plot(x, y4)
+axs[1,3].plot(x, y2*y4)
+
+
+#3rd row
+axs[2,0].plot(x, y3)
+axs[2,0].plot(x, y1)
+axs[2,0].plot(x, y3*y1)
+
+axs[2,1].plot(x, y3)
+axs[2,1].plot(x, y2)
+axs[2,1].plot(x, y3*y2)
+
+axs[2,2].plot(x, y3)
+axs[2,2].plot(x, y3)
+axs[2,2].plot(x, y3*y3)
+
+axs[2,3].plot(x, y3)
+axs[2,3].plot(x, y4)
+axs[2,3].plot(x, y3*y4)
+
+
+#4th row
+axs[3,0].plot(x, y4)
+axs[3,0].plot(x, y1)
+axs[3,0].plot(x, y4*y1)
+
+axs[3,1].plot(x, y4)
+axs[3,1].plot(x, y2)
+axs[3,1].plot(x, y4*y2)
+
+axs[3,2].plot(x, y4)
+axs[3,2].plot(x, y3)
+axs[3,2].plot(x, y4*y3)
+
+axs[3,3].plot(x, y4)
+axs[3,3].plot(x, y4)
+axs[3,3].plot(x, y4*y4)
+
+#plt.show()
