32620928a1006b2525bafc10fd48e09044eaccfb
Ethan White <ejwhite43@tntech.edu>
2025-03-09 20:20:35 -0500

    Draft of working program.

--- p2-NInt/ethanwhite/code/fcns.py
+++ p2-NInt/ethanwhite/code/fcns.py
@@ -1,67 +1,60 @@
 import numpy as np
-import math
-import scipy as sp # for legendre polynomial weight and roots
+import math # Used towards square-rooting things.
+import scipy as sp # For Legendre polynomial weight and roots
 
-
-# this is the function to be changed
-def f(x):
+def f(x): # The function that we are working with.
 	f = math.sin(math.sqrt(100*x))**2
 	return f
 
-# used in simpson rule
 def leftpoint(a,b,N):
 	sum, h = 0, (b-a)/N
-	x = a
+	x = a # Setting the left point at x = a.
 	for i in range(0,N-1):
-		x += h
-		sum += f(x) * h
+		x += h # Iterating by h.
+		sum += f(x) * h # Adding this new rectangle sum.
 
 	return sum
 
 def rightpoint(a,b,N):
 	sum, h = 0, (b-a)/N
-	x = a + h
+	x = a + h # Setting the point at x = a + h (the right of the leftpoint rectangle)
 	for i in range(0,N-1):
-		x += h
-		sum += f(x) * h
+		x += h # Iterating by h.
+		sum += f(x) * h # Adding this new rectangle sum.
 
 	return sum
 
 def midpoint(a,b,N):
 	sum, h = 0, (b-a)/N
-	x = a + h/2
+	x = a + h/2 # Finding the middle between left and right endpoints.
 	for i in range(0,N-1):
-		x += h
-		sum += f(x) * h
+		x += h # Iterating by h.
+		sum += f(x) * h # Adding this new rectangle sum.
 
 	return sum
 
 def trapezoid(a,b,N):
-        sum = (leftpoint(a,b,N) + rightpoint(a,b,N))/2
+        sum = (leftpoint(a,b,N) + rightpoint(a,b,N))/2 # Formula provided by homework.
         return sum
 
 def simpson(a,b,N):
-        sum = trapezoid(a,b,N)/3 + 2*midpoint(a,b,N)/3
+        sum = trapezoid(a,b,N)/3 + 2*midpoint(a,b,N)/3 # Formula provided by homework.
         return sum
 
-def uf(a,b,u):
-	ufunc = f(((b-a)*u+a+b)/2)
-	return ufunc
-
-def quadrature(a,b,N):
-	roots, weights = sp.special.roots_legendre(N)
+def quadrature(a,b,N): # Quadrature method!
+	roots, weights = sp.special.roots_legendre(N) # Provided on writeup.
 	sum = 0
 	for i in range(0,N):
-		sum += weights[i] * uf(a,b,roots[i])
-		if i == N-1: sum *= (b-a)/2
+		sum += weights[i] * f(((b-a)*roots[i]+a+b)/2) # Multiplies the weight by the function evaluated at optimal points. Essentially saying weights * f(x(u)).
+		if i == N-1: sum *= (b-a)/2 # Our du-component that we must use from our initial mapping.
 
 	return sum
 
-def usubtest(a,b,x):
+def usubtest(a,b,x): # Function used to test whether a and b truly map to -1 and 1.
 	usub = (2*x-a-b)/(b-a)
 	return usub
 
-def answer(L,R,M,T,S,Q):
+def answer(L,R,M,T,S,Q): # Just prints the integral values obtained from different methods.
 	print("\nThe correct value should be 1.0057025.\n")
 	print("The result of the integral, with the left point method, is:", L)
 	print("The result of the integral, with the right point method, is:", R)
@@ -71,7 +64,7 @@ def answer(L,R,M,T,S,Q):
 	print("The result of the integral, using the quadrature method, is:", Q)
 	return
 
-def init(a,b,N):
+def init(a,b,N): # Initializes variables to correspond to integral values.
 	L = leftpoint(a,b,N)
 	R = rightpoint(a,b,N)
 	M = midpoint(a,b,N)
@@ -81,14 +74,14 @@ def init(a,b,N):
 
 	return L,R,M,T,S,Q
 
-def methodcheck(a,b):
-	N, correct, error, i = 1, 1.005702, 1, 0
+def methodcheck(a,b): # Used towards creating the Simpson's rule N-table.
+	N, correct, error, i = 1, 1.0057025428, 1, 0
 	print("")
-	while error > 0.0000001:
-		val = simpson(a,b,N)
-		error = correct - val
-		print("N: ",N,", VALUE: ",f"{val:.{8}g}",", ERROR: ",f"{error:.{3}g}")
-		i += 1
-		N = 2 ** i
+	while error > 0.0000005: # For finding 6-significant digits accuracy.
+		val = simpson(a,b,N) # Calculating value at this N.
+		error = abs(correct - val) # Calculating error from actual value.
+		print("N: ",N,", VALUE: ",f"{val:.{8}g}",", ERROR: ",f"{error:.{3}g}") # Creates the n-table.
+		i += 1 # Iterating i.
+		N = 2 ** i # Iterating N = 2^i
 
 	return
--- p2-NInt/ethanwhite/code/p2.py
+++ p2-NInt/ethanwhite/code/p2.py
@@ -1,11 +1,11 @@
-import fcns
-import legendreplot
+import fcns # Python file of main computational functions.
+import legendreplot # Python file that spawns 4x4 Legendre polynomial plot.
 
-N, a, b = int(input("\nN = ")), 0, 2
-L,R,M,T,S,Q = fcns.init(a,b,N)
+N, a, b = int(input("\nN = ")), 0, 2 # Subinterval amount determined by user and setting the function bounds.
+L,R,M,T,S,Q = fcns.init(a,b,N) # Finding the integral sum using all six methods and throwing them back here.
 
-if fcns.usubtest(a,b,a) == -1 and fcns.usubtest(a,b,b) == 1: print("\n[a,b] maps to [-1,1].")
+if fcns.usubtest(a,b,a) == -1 and fcns.usubtest(a,b,b) == 1: print("\n[a,b] maps to [-1,1].") # Just verifying the u-sub maps to [-1,1].
 
-fcns.answer(L,R,M,T,S,Q)
-fcns.methodcheck(a,b)
-legendreplot.legendreplot()
+fcns.answer(L,R,M,T,S,Q) # Spits out the answer from the integrals found in line 5.
+fcns.methodcheck(a,b) # Creates the N-table using Simpson's rule.
+legendreplot.legendreplot() # Creates the 4x4 Legendre polynomial plot.
