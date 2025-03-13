0db78e269303bc5d420452ae376b301162889707
Ethan White <ejwhite43@tntech.edu>
2025-03-13 14:10:24 -0500

    P2, final

--- p2-NInt/ethanwhite/code/p2.py
+++ p2-NInt/ethanwhite/code/p2.py
@@ -1,11 +1,11 @@
-import fcns # Python file of main computational functions.
+import functions # Python file of main computational functions.
 import legendreplot # Python file that spawns 4x4 Legendre polynomial plot.
 
 N, a, b = int(input("\nN = ")), 0, 2 # Subinterval amount determined by user and setting the function bounds.
-L,R,M,T,S,Q = fcns.init(a,b,N) # Finding the integral sum using all six methods and throwing them back here.
+L,R,M,T,S,Q = functions.init(a,b,N) # Finding the integral sum using all six methods and throwing them back here.
 
-if fcns.usubtest(a,b,a) == -1 and fcns.usubtest(a,b,b) == 1: print("\n[a,b] maps to [-1,1].") # Just verifying the u-sub maps to [-1,1].
+if functions.usubtest(a,b,a) == -1 and functions.usubtest(a,b,b) == 1: print("\n[a,b] maps to [-1,1].") # Just verifying the u-sub maps to [-1,1].
 
-fcns.answer(L,R,M,T,S,Q) # Spits out the answer from the integrals found in line 5.
-fcns.methodcheck(a,b) # Creates the N-table using Simpson's rule.
+functions.answer(L,R,M,T,S,Q) # Spits out the answer from the integrals found in line 5.
+functions.methodcheck(a,b) # Creates the N-table using Simpson's rule.
 legendreplot.legendreplot() # Creates the 4x4 Legendre polynomial plot.
