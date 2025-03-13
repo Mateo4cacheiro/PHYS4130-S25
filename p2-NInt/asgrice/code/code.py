5a6a66dcceec0392232044c200e27c39aa7dbeaa
asgrice <asgrice13@gmail.com>
2025-03-13 14:33:40 -0500

    Updated writeup

--- p2-NInt/asgrice/code/code.py
+++ p2-NInt/asgrice/code/code.py
@@ -64,72 +64,91 @@ y4 = P4(x)
 #top row
 axs[0,0].plot(x, y1)
 axs[0,0].plot(x, y1)
-axs[0,0].plot(x, y1*y1)
+axs[0,0].plot(x, y1*y1, 'tab:red')
+axs[0,0].set_title('P1, P1, P1*P1')
 
 axs[0,1].plot(x, y1)
 axs[0,1].plot(x, y2)
-axs[0,1].plot(x, y1*y2)
+axs[0,1].plot(x, y1*y2, 'tab:red')
+axs[0,1].set_title('P1, P2, P1*P2')
 
 axs[0,2].plot(x, y1)
 axs[0,2].plot(x, y3)
-axs[0,2].plot(x, y1*y3)
+axs[0,2].plot(x, y1*y3, 'tab:red')
+axs[0,2].set_title('P1, P3, P1*P3')
 
 axs[0,3].plot(x, y1)
 axs[0,3].plot(x, y4)
-axs[0,3].plot(x, y1*y4)
+axs[0,3].plot(x, y1*y4, 'tab:red')
+axs[0,3].set_title('P1, P4, P1*P4')
 
 
 #2nd row
 axs[1,0].plot(x, y2)
 axs[1,0].plot(x, y1)
-axs[1,0].plot(x, y2*y1)
+axs[1,0].plot(x, y2*y1, 'tab:red')
+axs[1,0].set_title('P2, P1, P2*P1')
 
 axs[1,1].plot(x, y2)
 axs[1,1].plot(x, y2)
-axs[1,1].plot(x, y2*y2)
+axs[1,1].plot(x, y2*y2, 'tab:red')
+axs[1,1].set_title('P2, P2, P2*P2')
 
 axs[1,2].plot(x, y2)
 axs[1,2].plot(x, y3)
-axs[1,2].plot(x, y2*y3)
+axs[1,2].plot(x, y2*y3, 'tab:red')
+axs[1,2].set_title('P2, P3, P2*P3')
 
 axs[1,3].plot(x, y2)
 axs[1,3].plot(x, y4)
-axs[1,3].plot(x, y2*y4)
+axs[1,3].plot(x, y2*y4, 'tab:red')
+axs[1,3].set_title('P2, P4, P2*P4')
 
 
 #3rd row
 axs[2,0].plot(x, y3)
 axs[2,0].plot(x, y1)
-axs[2,0].plot(x, y3*y1)
+axs[2,0].plot(x, y3*y1, 'tab:red')
+axs[2,0].set_title('P3, P1, P3*P1')
 
 axs[2,1].plot(x, y3)
 axs[2,1].plot(x, y2)
-axs[2,1].plot(x, y3*y2)
+axs[2,1].plot(x, y3*y2, 'tab:red')
+axs[2,1].set_title('P3, P2, P3*P2')
 
 axs[2,2].plot(x, y3)
 axs[2,2].plot(x, y3)
-axs[2,2].plot(x, y3*y3)
+axs[2,2].plot(x, y3*y3, 'tab:red')
+axs[2,2].set_title('P3, P3, P3*P3')
 
 axs[2,3].plot(x, y3)
 axs[2,3].plot(x, y4)
-axs[2,3].plot(x, y3*y4)
+axs[2,3].plot(x, y3*y4, 'tab:red')
+axs[2,3].set_title('P3, P4, P3*P4')
 
 
 #4th row
 axs[3,0].plot(x, y4)
 axs[3,0].plot(x, y1)
-axs[3,0].plot(x, y4*y1)
+axs[3,0].plot(x, y4*y1, 'tab:red')
+axs[3,0].set_title('P4, P1, P4*P1')
 
 axs[3,1].plot(x, y4)
 axs[3,1].plot(x, y2)
-axs[3,1].plot(x, y4*y2)
+axs[3,1].plot(x, y4*y2, 'tab:red')
+axs[3,1].set_title('P4, P2, P4*P2')
 
 axs[3,2].plot(x, y4)
 axs[3,2].plot(x, y3)
-axs[3,2].plot(x, y4*y3)
+axs[3,2].plot(x, y4*y3, 'tab:red')
+axs[3,2].set_title('P4, P3, P4*P3')
 
 axs[3,3].plot(x, y4)
 axs[3,3].plot(x, y4)
-axs[3,3].plot(x, y4*y4)
+axs[3,3].plot(x, y4*y4, 'tab:red')
+axs[3,3].set_title('P4, P4, P4*P4')
 
-#plt.show()
+for ax in fig.get_axes():
+    ax.label_outer()
+
+plt.show()
